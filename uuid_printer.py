40cbc48eab28c952a2a19bfc2646bffc3018ac59
KarolNi <git@karolni.pl>
2021-04-10 10:56:36 +0200

    added functions to calculate number of codes on page

--- uuid_printer.py
+++ uuid_printer.py
@@ -1,20 +1,80 @@
 import uuid
 import segno
 import fpdf
+from functools import lru_cache
 
-config = {"dpi":72}
 
-pdf = fpdf.FPDF(orientation="P", unit="mm", format="A4")
+config = {"dpi":100,
+"margins":10,
+"error":"H",
+"spacing":0.5,
+"font_size":5,
+"orientation":"L",
+"format":"A4",
+}
+
+def pt2mm(pt: float) -> float:
+    return 0.35277777777778*pt
+
+# size of cell
+
+@lru_cache
+def size_of_qr() -> [float, float]:
+    qrcode = segno.make(int(0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF), error="H")  # 128 bit number
+    qrimage = qrcode.to_pil()
+    wqr = qrimage.width/(config["dpi"])*25.4
+    hqr = qrimage.height/(config["dpi"])*25.4
+    return [wqr, hqr]
+
+@lru_cache
+def size_of_text() -> [float, float]:
+    pdf = fpdf.FPDF()
+    pdf.set_font('courier', '', config["font_size"])
+    wt = pdf.get_string_width("12345678")  # 8 glyphs
+    ht = pt2mm(config["font_size"])
+    return [wt, ht]
+
+def size_of_cell() -> [float, float]:
+    w = max(size_of_qr()[0],size_of_text()[0]) + config["spacing"]
+    h = size_of_qr()[1] + size_of_text()[1] + config["spacing"]
+    return [w, h]
+
+# number of cells
+
+def size_of_page() -> [float, float]:
+    pdf = fpdf.FPDF(orientation=config["orientation"], unit="mm", format=config["format"])
+    pdf.add_page()
+    return [pdf.w, pdf.h]
+
+@lru_cache
+def size_of_printable() -> [float, float]:
+    [w, h] = size_of_page()
+    return [w - 2 * config["margins"], h - 2 * config["margins"]]
+
+def number_of_cells() -> [int, int]:
+    w = size_of_printable()[0] // size_of_cell()[0]
+    h = size_of_printable()[1] // size_of_cell()[1]
+    return [int(w), int (h)]
+
+# table of uuids
+
+print (number_of_cells())
+
+
+pdf = fpdf.FPDF(orientation="L", unit="mm", format="A4")
 pdf.add_page()
 pdf.set_font('courier', '', 5)
 
 x = 10
 y = 10
+
+pdf.line(config["margins"], config["margins"], config["margins"], pdf.h - config["margins"])
+
 for i in range(1,10):
 
     uuid1 = uuid.uuid4()
 
-    s = segno.make(uuid1.int)
+    s = segno.make(uuid1.int, error="H")
     sp = s.to_pil()
     w = sp.width/(i*20)*25.4
 
